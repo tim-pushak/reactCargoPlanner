734620477bdccde6a73ec8e1f0603529fa6115a2
Artem <pushakartem@gmail.com>
2022-07-26 13:44:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import Shipment from './shipment.json';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {
+        Shipment.map(shipment => {
+          return (
+            <div className="box" key={shipment.id}>
+              <header class="main__header">
+                <section class="main__header_wrap">
+                  <div class="main__header_h1">
+                    <h1>Cargo Planner</h1>
+                  </div>
+                  <div class="searchBar">
+                    <form id="searchbar" action="#">
+                      <input class="searchbar_input" type="text" placeholder="Search"/>
+                    </form>
+                  </div>
+
+                  <div class="main__header_btns">
+                    <button class="load_btn btns">Load</button>
+                    <button class="save_btn btns">Save</button>
+                  </div>
+                </section>
+              </header>
+
+              <main className="containerr">
+                <section class="scrollBar1 companies">
+                  <div>
+                    <p><a href={shipment.name}>{shipment.name}</a></p>
+                  </div>
+                </section>
+                <section className="scrollBar2 company_details">
+                  <div className="company_title">
+                    <h1>{shipment.name}</h1>
+                  </div> 
+                  <div class="company_email">
+                    <p><a href="mailto:{shipment.email}">{shipment.email}</a></p>
+                  </div> <br />
+                  <div class="bays">
+                    <p>
+                      Number of required cargo bays <strong>2</strong>
+                    </p>
+                  </div>
+                  <div class="cargo_boxes">
+                  <p>Cargo boxes</p>
+                  <input type="text" value={shipment.boxes}></input>
+                  </div>
+                </section>
+              </main>
+
+              <footer class="main_footer">
+                <p></p>
+              </footer>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
